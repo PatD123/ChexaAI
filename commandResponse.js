aa1a5b3287cf61eead7c91b1cd2c30abaa1eab2f
Patrick Dai <patrickdai917@gmail.com>
2023-08-04 23:55:25 -0400

    - Edited styling of the site a little bit more. - Text-To-Speech handling after Chat response is decently done.

--- commandResponse.js
+++ commandResponse.js
@@ -19,6 +19,48 @@ function getGPTResponse(event){
         return response.text();
     }).then(function (response){
         console.log(response)
-        result.innerHTML = "Response: " + response;
+        result.innerHTML = response;
+        text_to_speech(response);
     })
+}
+
+// text_to_speech("You are so very");
+
+function text_to_speech(response){
+    // Microsoft David - English (United States) (en-US) — DEFAULT
+    const synth = window.speechSynthesis;
+    const speech = new SpeechSynthesisUtterance(response);
+
+    let voices = [];
+    let s = setVoices(synth);
+    s.then((vcs) => {
+        voices = vcs;
+        // Microsoft David - English (United States)
+        var i = 0;
+        for(; i<voices.length; i++){
+            if(voices[i].name === "Microsoft Ryan Online (Natural) - English (United Kingdom)") {
+                break;
+            }
+        }
+        speech.voice = voices[i];
+
+        speech.pitch = 120;
+        speech.rate = 1;
+        synth.speak(speech);
+    }); 
+}
+
+function setVoices(synth){
+    return new Promise(
+        function (resolve, reject) {
+
+            id = setInterval(() => {
+                if (synth.getVoices().length !== 0) {
+                    resolve(synth.getVoices());
+                    clearInterval(id);
+                }
+            }, 10);
+
+        }
+    )
 }
