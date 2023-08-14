40358ea19cbb1ccd94c9da45a0cdd78916a9b5e9
Patrick Dai <patrickdai917@gmail.com>
2023-08-13 22:17:12 -0700

    Added buzzer/speaker capability.

--- py_2_Arduino/py_2_Arduino.ino
+++ py_2_Arduino/py_2_Arduino.ino
@@ -1,4 +1,5 @@
 int pinLED = 11;
+int pinBuzzer = 10;
 String arr[3] = {"", "", ""};
 int arrSize = 3;
 
@@ -15,10 +16,17 @@ void loop() {
     Serial.println(arr[i]);
   }
 
-  // LED FEATURE
+  // LED Feature
   if(arr[0] == "max")analogWrite(pinLED, (255./4. * 4.));
   else if(arr[0] == "high")analogWrite(pinLED, (255./4. * 3.));
   else if(arr[0] == "medium")analogWrite(pinLED, (255./4. * 2.));
   else if(arr[0] == "low")analogWrite(pinLED, (255./4. * 1.));
   else analogWrite(pinLED, 0);
+
+  // Buzzer/Volume Feature
+  if(arr[0] == "max")analogWrite(pinBuzzer, (255./4. * 4.));
+  else if(arr[0] == "high")analogWrite(pinBuzzer, (255./4. * 3.));
+  else if(arr[0] == "medium")analogWrite(pinBuzzer, (255./4. * 2.));
+  else if(arr[0] == "low")analogWrite(pinBuzzer, (255./4. * 1.));
+  else analogWrite(pinBuzzer, 0);
 }
